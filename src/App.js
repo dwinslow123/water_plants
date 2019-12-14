257e0711694c83ef308960e0f8b467ac049c1901
Dan Winslow <Dwinslow123@me.com>
2019-12-14 15:00:05 -0600

    json file appearing as list on screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as data from './plants.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    plantList: [],
+  }
+  
+  componentDidMount() {
+    this.setState({
+      plantList: data.default
+    });
+  };
+
+  render() {
+    const { plantList } = this.state;
+
+    const renderPlants =  plantList.map((plant, i) => {
+          return (
+            <ul key={i}>
+              <li>{plant.name}</li>
+              <li>{plant.water_after}</li>
+            </ul>
+          )
+        })
+
+    return (
+      <div>
+        {renderPlants}
+      </div>
+    )
+  }
+
 }
 
 export default App;
