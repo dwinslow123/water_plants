11447f16ee58d53b3fc1144c17d5788eb903742a
Dan Winslow <Dwinslow123@me.com>
2019-12-14 20:44:15 -0600

    added functions to change the date to one day previous / forward

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import * as data from './plants.json';
 
 class App extends Component {
   state = {
     plantList: [],
-  }
+    date: new Date('December 16, 2019 00:00:00')
+  };
+
+  handleNewDate = (date) => {
+    this.setState({
+      date: date
+    });
+  };
+
+  handleYesterdayDate = (date) => {
+    date = this.state.date;
+    date.setDate(date.getDate() -1);
+    this.setState({
+      date: date
+    });
+  };
+
+  handleTomorrowDate = (date) => {
+    date = this.state.date;
+    date.setDate(date.getDate() + 1);
+    this.setState({
+      date: date
+    });
+  };
   
+  handleNameClick = (e) => {
+    this.setState({
+      plantList: e.target.value,
+    });
+  };
+
   componentDidMount() {
     this.setState({
       plantList: data.default
@@ -25,9 +56,16 @@ class App extends Component {
           )
         })
 
+
     return (
       <div>
+        <DatePicker
+          selected={ this.state.date }
+          onChange={ this.handleNewDate }
+        />
         {renderPlants}
+        <button onClick={ this.handleYesterdayDate }>Yesterday</button>
+      <button onClick={ this.handleTomorrowDate }>Tomorrow</button>
       </div>
     )
   }
