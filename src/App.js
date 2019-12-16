ee77edfca78bc88a7e20b619f92d451a091253a8
Dan Winslow <Dwinslow123@me.com>
2019-12-16 17:41:31 -0600

    added some unit tests, working on styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
-import './App.css';
 import 'react-datepicker/dist/react-datepicker.css';
+import ListGroup from 'react-bootstrap/ListGroup';
 import * as data from './plants.json';
+
+import './custom.scss';
+import './App.scss';
 
 class App extends Component {
   state = {
@@ -32,12 +35,6 @@ class App extends Component {
     });
   };
   
-  handleNameClick = (e) => {
-    this.setState({
-      plantList: e.target.value,
-    });
-  };
-
   componentDidMount() {
     this.setState({
       plantList: data.default
@@ -46,35 +43,36 @@ class App extends Component {
 
   render() {
     const { plantList, date } = this.state;
-
     const renderPlants = plantList.map((plant, i) => {
       const startDate = new Date('December 16, 2019 00:00:00');
       const today = (date.getDate() % startDate.getDate());
+
       if (date.getDay() === 0 || date.getDay() === 6) {
         return null;
-      } else if ((date.getDay() - 1 === 0 || date.getDay() +1 === 6) || (today % parseInt(plant.water_after) === 0)) {
+      } else if (((date.getDay() - 1 === 0) || (date.getDay() +1 === 6)) || (today % parseInt(plant.water_after) === 0)) {
         return (
-          <ul key={i}>
-            <li>{plant.name}</li>
-          </ul>
+          <ListGroup>
+            <ListGroup.Item>{plant.name}</ListGroup.Item>
+          </ListGroup>
         )
-      } 
+      }
       return null;
     })
 
     return (
-      <div>
+      <div class="main-app">
+        <div class="date-nav">
+        <button onClick={ this.handleYesterdayDate }>Yesterday</button>
         <DatePicker
           selected={ this.state.date }
           onChange={ this.handleNewDate }
         />
-          {renderPlants}
-        <button onClick={ this.handleYesterdayDate }>Yesterday</button>
       <button onClick={ this.handleTomorrowDate }>Tomorrow</button>
+      </div>
+          {renderPlants}
       </div>
     )
   }
-
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { shallow, mount, be } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter() });
+
+test('Click Yesterday button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(getByText('Yesterday'));
+});
+
+test('Click Tomorrow Button', () => {
+  const { getByText } = render(<App />);
+  fireEvent.click(getByText('Tomorrow'));
+})
+
+describe('App renders correctly', () => {
+  it ('renders correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
